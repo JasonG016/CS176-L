dcca69aebc63f822a21ee765cd0944656fc62577
JasonG016 <114097282+JasonG016@users.noreply.github.com>
2023-09-12 12:03:56 -0400

    my first java program v2

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-// CS176L
-// Jason Galvao
-// helloWorld
+/* CS176L
+ Jason Galvao
+ helloWorld */
 public class hello {
 	public static void main(String[] args) {
-	    System.out.println("Hello World");
+	    System.out.println("Hello World this is Jason Galvao");
 	}
 }
