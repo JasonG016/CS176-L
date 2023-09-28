29984064004e722fce979ffbed3f8e888a5f9bf4
JasonG016 <114097282+JasonG016@users.noreply.github.com>
2023-09-28 10:19:10 -0400

    Microwave

---
+++ MicrowaveHW/MicrowaveTester.java
@@ -0,0 +1,13 @@
+package MicrowaveHW;
+
+class MicrowaveTester {
+    public static void main(String[] args) {
+        Microwave myMicrowave = new Microwave();
+
+        myMicrowave.timeButton();
+        myMicrowave.timeButton();
+        myMicrowave.powerButton();
+        myMicrowave.startButton();
+        myMicrowave.resetButton();
+    }
+}
