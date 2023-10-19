6ab95f6d5fb79227f024fe4df8f6bbef8d809621
JasonG016 <114097282+JasonG016@users.noreply.github.com>
2023-10-19 16:03:51 -0400

    Person Inheritance Assignment

---
+++ Person.java
@@ -0,0 +1,32 @@
+package person;
+
+/**
+A person is represented by the name and a birth year.
+You must enter the class name, instance variable(s), and constructor below
+*/
+
+public class Person {
+    private String name;
+    private int birthYear;
+
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+/**
+   Returns the string representation of the object.
+   @return a string representation of the object
+*/
+public String toString()
+{
+   return "Person[name=" + name + ",birthYear=" + birthYear + "]";
+}
+}
---
+++ Student.java
@@ -0,0 +1,28 @@
+package person;
+
+/**
+A student is represented by the name, birth year, and major.
+You must enter the class name, instance variable(s), and constructor below
+*/
+
+public class Student extends Person {
+    private String major;
+
+    public Student(String name, int birthYear, String major) {
+        super(name, birthYear);
+        this.major = major;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+/**
+   Returns the string representation of the object.
+   @return a string representation of the object
+*/
+public String toString()
+{
+   return "Student[super=" + super.toString() + ",major=" + major + "]";
+}
+}
